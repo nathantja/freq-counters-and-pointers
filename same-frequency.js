7257ffda626c1efa0cea62823c1c519b5b180a96
Nathan Tjahjadi <nathantja@yahoo.com>
2023-08-11 18:05:27 -0500

    finished same-frequency and two-array-object

--- same-frequency.js
+++ same-frequency.js
@@ -1,3 +1,45 @@
-// add whatever parameters you deem necessary & write docstring
-function sameFrequency() {
+"use strict";
+
+// use frequency counter on both arguments
+// compare one counter to the other
+// make sure length is the same
+
+/**
+ * Function that creates an object counter for an iterable.
+ * @param {Iterable} items
+ * @returns object
+ */
+function getFrequencyCounter(items) {
+  const frequency = {};
+
+  for (const item of items) {
+    const currentValue = frequency[item] || 0;
+    frequency[item] = currentValue + 1;
+  }
+
+  return frequency;
+
+}
+
+/**
+ * Function that given two positive integers, finds out if the two numbers have
+ * the same frequency of digits.
+ * @param {number} num1
+ * @param {number} num2
+ * @returns true or false
+ */
+function sameFrequency(num1, num2) {
+  const num1String = num1.toString();
+  const num2String = num2.toString();
+  if (num1String.length !== num2String.length) return false;
+
+  const num1Freq = getFrequencyCounter(num1String);
+  const num2Freq = getFrequencyCounter(num2String);
+
+  for (const key in num1Freq) {
+    if (num1Freq[key] !== num2Freq[key]) {
+      return false;
+    }
+  }
+  return true;
 }
--- two-array-object.js
+++ two-array-object.js
@@ -1,3 +1,31 @@
-// add whatever parameters you deem necessary & write docstring
-function twoArrayObject() {
+"use strict";
+
+
+// function that accepts two arrays array 1 contains "keys" array 2 contains
+// "values" function returns object created from both arrays
+//
+// initialize empty object iterate through first array of keys, then set
+// object.key value to the value array
+// if value is undefined, make sure to set key/value to null
+// return object at the end
+
+
+
+/** function accepts two arrays, one with keys and one with values. Returns
+ * object with keys and values. If array value is undefined, resulting key is
+ * set to null.
+ * @param {Array} arrayOfKeys array with values for keys
+ * @param {Array} arrayOfValues array with values for values
+ * @Returns object
+ */
+function twoArrayObject(arrayOfKeys, arrayOfValues) {
+  const object = {};
+
+  for (let i = 0; i < arrayOfKeys.length; i++) {
+    const key = arrayOfKeys[i];
+    const value = arrayOfValues[i] || null;
+
+    object[key] = value;
+  }
+  return object;
 }
