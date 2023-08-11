ea4827ae9675b3a51a272158f5a6fb974d824d69
Daniel Zych <danjzych@gmail.com>
2023-08-11 17:35:21 -0500

    Completed average pair and can-construct-word

--- average-pair.js
+++ average-pair.js
@@ -1,4 +1,38 @@
+"use strict";
+
+//in this function, we receive an sorted array of numbers, as well as a targeted average.
+//we want to go through the array, and see if there are any pairs, where their average
+//is equal to the target average, and if so we return true
+
 // add whatever parameters you deem necessary & write docstring
-function averagePair() {
+function averagePair(nums, targetAvg) {
+
+  //we will use multiple pointers strategy
+  //begin by ideclaring a left and right pointer
+  //while the left is lower than the right, we will look at the pointers, and see if their average is
+  //equal to target average. If so, we return true. If not, we will then decrement the right pointer, if the
+  //average is too high.
+  //we will increment the left if too low,
+  //if we get outside of the while loop, we will return false
+
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (left < right) {
+
+    const currentAvg = (nums[left] + nums[right]) / 2;
+
+    if (currentAvg === targetAvg) {
+      return true;
+    } else if (currentAvg > targetAvg) {
+      right--;
+    } else if (currentAvg < targetAvg) {
+      left++;
+    }
+
+  }
+
+  return false;
+
 }
 
--- can-construct-word.js
+++ can-construct-word.js
@@ -1,3 +1,47 @@
+"use strict";
+//we will receive a word and letters, we need to check to see if the word
+//can be spelled with the letters passed in
+//our inputs will only be lowercase letters
+//return a boolean
+
 // add whatever parameters you deem necessary & write doc comment
-function canConstructWord() {
+
+//TODO:add docstrings
+function getFrequencyCounter(items) {
+
+  //declare an object literal where we will count the frequency of our letters
+  //iterate through 'items', if a key already exists for item, incrment, if not, create key and set to one
+  //return objectliteral
+
+  const frequency = {};
+
+  for (const item of items) {
+    const currentValue = frequency[item] || 0;
+    frequency[item] = currentValue + 1;
+  }
+
+  return frequency;
+
+}
+
+function canConstructWord(word, letters) {
+  //input word and letters
+  //for each letter in the word, we will want to check to see if this letter
+  //is in letters as well
+  //if not return false
+  //if were get to the end of word without issue, return true
+
+  const wordFreqs = getFrequencyCounter(word);
+  const lettersFreqs = getFrequencyCounter(letters);
+
+  for (const letter in wordFreqs) {
+
+    if (!(letter in lettersFreqs) || lettersFreqs[letter] < wordFreqs[letter]) {
+      return false;
+    }
+
+  }
+
+  return true;
+
 }
