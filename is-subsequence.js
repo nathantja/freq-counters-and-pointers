9b0a0972e8aa5d0c145b195ceac62aa2c5b19d33
Nathan Tjahjadi <nathantja@yahoo.com>
2023-08-11 19:31:27 -0500

    unfinished is-subsequence.js

--- is-subsequence.js
+++ is-subsequence.js
@@ -1,3 +1,25 @@
+// function that checks if s1 string is inside s2 string
+// order matters for s1
+//
+// pointer on s1
+// pointer on s2
+//
+
+
+
 // add whatever parameters you deem necessary & write docstring
-function isSubsequence() {
+function isSubsequence(s1, s2) {
+  let pointer1 = 0;
+  let pointer2 = 0;
+
+  for (let i = 0; i < s1.length; i++) {
+    for (let j = 0; j < s2.length; j++) {
+      if (s1[i] === s2)
+    }
+    if (s1[i] === s2)
+
+  }
+
+
+
 }
